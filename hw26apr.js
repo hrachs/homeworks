1e0ace029f43e100f248ff22822a223faf518a99
Hrach <youremail@example.com>
2025-04-28 09:56:50 +0400

    update

---
+++ hw26apr.js
@@ -0,0 +1,55 @@
+// hw 1
+
+// const prompt = require('prompt-sync')();
+
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
+
+// function greet(name){
+//     return `Hello ${name}`;
+// }
+
+// let num1 =Number( prompt("Enter num1  "));
+// let num2 =Number( prompt("Enter num2  "));
+
+// console.log(multiply(num1, num2));
+
+// let name = prompt("Enter name   ");
+// console.log(greet(name));
+
+// hw 2
+// const prompt = require('prompt-sync')();
+
+// const person ={
+//     firstName: "Hrach",
+//     lastName: "Sahakyan",
+//     age: "21",
+//     introduce: function (){
+//         return `Hi I'm ${this.firstName} ${this.lastName} and I'm ${this.age} years old.`
+//     }
+// }
+
+// person.firstName=prompt("Enter Name:  ");
+// person.lastName=prompt("Enter surename:  ");
+// person.age=Number(prompt("Enter age: "));
+// console.log(person.introduce());
+
+
+// hw 3
+
+// const person ={
+//     firstName: "Hrach",
+//     lastName: "Sahakyan",
+//     age: "21",
+//     introduce: function (){
+//         return `Hi I'm ${this.firstName} ${this.lastName} and I'm ${this.age} years old.`
+//     },
+//     showThis: function(){console.log(this)},
+// }
+
+// person.showThis();
+// person.showThis;
+
+// hw 4
+
