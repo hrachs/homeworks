840018b08a89e53610fbfb1ebc6391e6394f3cf3
Hrach Sahakyan <hsahakyan2003@gmail.com>
2025-05-03 20:14:32 +0400

    update

--- hw3may.js
+++ hw3may.js
@@ -3,6 +3,8 @@
 // setTimeout(delayedMessage, 3000);
 // console.log("Starting...");
 
+const { response, text } = require("express");
+
 // hw 2
 
 // const prompt = require('prompt-sync')();
@@ -25,5 +27,21 @@
 
 // hw 3 
 
+// fetch('https://jsonplaceholder.typicode.com/posts')
 
+// .then(response=>{
+//     return response.json();
+// })
+// .then(data =>{
+//     console.log(data[0].title);
+// })
+
+
+// function simulateDownlaod(filename, callback){
+//     setTimeout(function (){
+//         callback("download complere: " + filename);
+//     },2000);
+// };
+
+// simulateDownlaod("file.txt", text =>{console.log(text)});
 
