a88dfb22988c2afaab3cd33fb2a766d0c56fb1aa
Hrach <youremail@example.com>
2025-05-03 19:27:21 +0400

    update

---
+++ hw3may.js
@@ -0,0 +1,29 @@
+//hw 1
+// function delayedMessage(){console.log("This message is delayed")};
+// setTimeout(delayedMessage, 3000);
+// console.log("Starting...");
+
+// hw 2
+
+// const prompt = require('prompt-sync')();
+
+// function foo(number){
+//     const id = setInterval(()=> {
+//         console.log(number);
+//         number--;
+//     if(number === 0){
+//         clearInterval(id);
+//         console.log("Time's up!");
+//     }
+//     },1000);
+
+// }
+
+// let num = prompt("Enter number:  ");
+
+// foo(num);
+
+// hw 3 
+
+
+
